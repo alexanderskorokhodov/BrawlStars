d82eb656ed45a1f8c372719c56e8707b6aefcfc3
alexanderskorokhodov <a9169590391@gmail.com>
2022-01-21 23:04:09 +0300

    фикс карты

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import menuWindow
 import startWindow
 
 
-run = True
-while run:
-    run, sock, login, password = startWindow.main()
-    run, sock, extra_message = menuWindow.main(sock, login, password)
-    if run:
-        import mainGame
-        mainGame.main(sock, extra_message, login)
+try:
+    run = True
+    while run:
+        run, sock, login, password = startWindow.main()
+        run, sock, extra_message = menuWindow.main(sock, login, password)
+        if run:
+            import mainGame
+            mainGame.main(sock, extra_message, login)
+except:
+    pass
